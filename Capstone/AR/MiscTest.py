0222cf92d6d32e28255efc8fe670095dc8add660
Toni Odetayo <oluwatoni_oficial@Tonis-MacBook-Pro.local>
2025-03-10 17:26:06 -0400

    AR_Mar10_1725

--- Capstone/AR/MiscTest.py
+++ Capstone/AR/MiscTest.py
@@ -1,4 +1,13 @@
-import viewer_control2
+# import viewer_control2
 
-headPosition = viewer_control2()
-print("Head Position:", headPosition)
+# headPosition = viewer_control2()
+# print("Head Position:", headPosition)
+
+# import cv2
+# cv2.imshow("Frame", image)
+# cv2.waitKey(0)
+
+from OpenGL.GL import *
+print("OpenGL is working!")
+
+
